cd776fa268722a8749ce2e3d5001d98415f86362
pingu0130 <penguin1998@daum.net>
2023-04-16 15:50:29 +0900

    패키지 추가

--- JWH/demo/src/main/java/com/example/demo/service/ServiceImpl.java
+++ JWH/demo/src/main/java/com/example/demo/service/ServiceImpl.java
@@ -1,4 +1,34 @@
 package com.example.demo.service;
 
-public class ServiceImpl {
+import com.example.demo.db.BlogEntity;
+
+import java.util.List;
+
+public class ServiceImpl implements Service{
+
+
+    @Override
+    public BlogEntity createBlog() {
+        return null;
+    }
+
+    @Override
+    public BlogEntity updateBlog() {
+        return null;
+    }
+
+    @Override
+    public void deleteBlog() {
+
+    }
+
+    @Override
+    public List<BlogEntity> readList() {
+        return null;
+    }
+
+    @Override
+    public BlogEntity readOne() {
+        return null;
+    }
 }
