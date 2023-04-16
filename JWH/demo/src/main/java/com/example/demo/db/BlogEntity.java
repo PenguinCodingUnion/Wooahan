26013be42d56d97c1333486201c63ca62ed24747
pingu0130 <penguin1998@daum.net>
2023-04-16 16:08:39 +0900

    조합장님 만세

--- JWH/demo/src/main/java/com/example/demo/db/BlogEntity.java
+++ JWH/demo/src/main/java/com/example/demo/db/BlogEntity.java
@@ -1,8 +1,12 @@
 package com.example.demo.db;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "blog")
 public class BlogEntity {
     @Id
     @Column(name = "blogId")
