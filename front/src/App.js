2d60d3e4dcc78209abf9f2d1e5300f252fdcdc7b
신현광 <rhkd0363@gmail.com>
2023-04-24 18:00:03 +0900

    [Fix] : import 경로 수정

--- front/src/App.js
+++ front/src/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import { Counter } from "./store/features/counter/Counter";
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
@@ -53,6 +53,6 @@ function App() {
       </header>
     </div>
   );
-}
+};
 
 export default App;
