8e0010e1faa63e83a79fd3aaec09e77ac8f0f63a
Heesoo Choi <102863300+heesootory@users.noreply.github.com>
2023-04-24 16:21:13 +0900

    [Dir]ÄÄÆ÷³ÍƮ ±¸Á¶¼³°è »󼼳»¿ë

---
+++ front/src/pages/GameBubble.jsx
@@ -0,0 +1,19 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+import { connect } from 'react-redux'
+
+export const GameBubble = (props) => {
+  return (
+    <div>GameBubble</div>
+  )
+}
+
+GameBubble.propTypes = {
+  second: PropTypes.third
+}
+
+const mapStateToProps = (state) => ({})
+
+const mapDispatchToProps = {}
+
+export default connect(mapStateToProps, mapDispatchToProps)(GameBubble)
