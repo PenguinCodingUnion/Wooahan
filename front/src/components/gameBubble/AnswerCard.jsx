f14bcf17fcefd44c15d11f4029022ea3b0be59c6
Jihoon <kjh8673a@naver.com>
2023-04-28 17:57:28 +0900

    [Feat] 카드 사이즈 수정

--- front/src/components/gameBubble/AnswerCard.jsx
+++ front/src/components/gameBubble/AnswerCard.jsx
@@ -16,16 +16,16 @@ export const AnswerCard = (props) => {
 
   return (
     <div className="w-screen h-screen">
-      <div className="absolute grid -mt-48 -ml-40 overflow-hidden bg-white border-4 border-black justify-items-center h-96 rounded-3xl top-1/2 left-1/2 w-80">
+      <div className="absolute grid -mt-40 -ml-40 overflow-hidden bg-white border-4 border-black justify-items-center h-80 rounded-3xl top-1/2 left-1/2 w-80">
         <div
-          className="border h-72 w-80"
+          className="h-64 overflow-hidden w-80"
           style={{
             backgroundImage: `url(${props.image})`,
             backgroundSize: "cover",
             backgroundRepeat: "no-repeat",
           }}
         ></div>
-        <div className="h-8 text-5xl font-bold leading-8 font-netmarbleB">
+        <div className="h-16 text-5xl font-bold leading-16 font-netmarbleB">
           {props.name}
         </div>
       </div>
