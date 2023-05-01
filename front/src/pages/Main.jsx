a07847c74f0933ed70d136a78f702b4d0973e2fd
Jihoon <kjh8673a@naver.com>
2023-05-01 09:43:34 +0900

    Revert "[Feat] 고 버블"
    This reverts commit a2f0457e6affc182295aacfdc9fbf6802bacb8db.

--- front/src/pages/Main.jsx
+++ front/src/pages/Main.jsx
@@ -1,18 +1,14 @@
-import PropTypes from "prop-types";
-import React from "react";
-import { connect } from "react-redux";
+import PropTypes from 'prop-types'
+import React from 'react'
+import { connect } from 'react-redux'
 import { Link } from "react-router-dom";
 
 export const Main = (props) => {
   return (
     <div>
-      <h1>Main페이지</h1>
+      <h1>Main</h1>
       <Link to={"jump"}>고 짬프</Link>
-      <br />
       <Link to={"sleigh"}>고 썰매</Link>
-      <br />
-      <Link to={"bubble"}>고 버블</Link>
-      <br />
     </div>
   );
 };
@@ -21,8 +17,8 @@ Main.propTypes = {
   // second: PropTypes.third
 };
 
-const mapStateToProps = (state) => ({});
+const mapStateToProps = (state) => ({})
 
-const mapDispatchToProps = {};
+const mapDispatchToProps = {}
 
-export default connect(mapStateToProps, mapDispatchToProps)(Main);
+export default connect(mapStateToProps, mapDispatchToProps)(Main)
