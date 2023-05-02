e723e7ba06679b3b7c150682166cc82bb5944b82
신현광 <rhkd0363@gmail.com>
2023-05-02 12:48:30 +0900

    [Add] Sleigh Slice 추가

--- front/src/store/store.js
+++ front/src/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "./features/counter/counterSlice";
+import sleighReducer from "./features/sliegh/sleighSlice";
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    sleigh: sleighReducer,
   },
 });
