7a1b84b9baa57ed9d6ab1d14e15b4e10c09a894f
Jihoon <kjh8673a@naver.com>
2023-05-02 11:02:28 +0900

    [Feat] 방울 애니메이션 진행중

--- front/src/components/gameBubble/WordBubble.jsx
+++ front/src/components/gameBubble/WordBubble.jsx
@@ -1,21 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { connect } from "react-redux";
 
 import bgBubble from "assets/images/structure_bubble.png";
 import classNames from "classnames";
 
 export const WordBubble = (props) => {
+  const [doBubblePop, setDoBubblePop] = useState("");
   const classStr = classNames(
     "absolute grid items-center text-3xl font-extrabold justify-items-center font-MaplestoryBold",
     props.positionX,
     props.positionY,
     props.size,
-    props.animation
+    props.animation,
+    doBubblePop
   );
 
   const clickAnswer = () => {
     if (props.clickAnswer) {
-      props.clickAnswer(props.number);
+      setDoBubblePop("animate-bubblePop");
+      setTimeout(() => {
+        props.clickAnswer(props.number);
+      }, 500);
     }
   };
   return (
