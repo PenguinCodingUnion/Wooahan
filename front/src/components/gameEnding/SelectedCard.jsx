3738addf0330d201d5b17d3404c881383c75496d
Jihoon <kjh8673a@naver.com>
2023-05-04 10:12:27 +0900

    [Feat] 보상 카드 선택 컴포넌트 분리

--- front/src/components/gameEnding/SelectedCard.jsx
+++ front/src/components/gameEnding/SelectedCard.jsx
@@ -1,28 +1,13 @@
-import React, { useEffect, useState } from "react";
-import ReactAudioPlayer from "react-audio-player";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
-import correct from "assets/sounds/correct.wav";
-import wrong from "assets/sounds/wrong.wav";
 
 export const AnswerCard = (props) => {
-  const [sound, setsound] = useState(<></>);
-
   useEffect(() => {
-    if (props.name === props.answer) {
-      setTimeout(() => {
-        props.closeCard();
-        props.changeQuiz();
-      }, 1500);
-      setsound(<ReactAudioPlayer src={correct} autoPlay volume={1} />);
-    } else if (props.name !== props.answer) {
-      setTimeout(() => props.closeCard(), 1500);
-      setsound(<ReactAudioPlayer src={wrong} autoPlay volume={1} />);
-    }
+    setTimeout(() => props.closeCard(), 1500);
   }, [props]);
 
   return (
     <div className="w-screen h-screen">
-      <div>{sound}</div>
       <div className="absolute grid -mt-40 -ml-40 overflow-hidden border-4 border-mainBlack bg-mainWhite justify-items-center h-80 rounded-3xl top-1/2 left-1/2 w-80">
         <div
           className="h-64 overflow-hidden w-80"
@@ -33,7 +18,7 @@ export const AnswerCard = (props) => {
           }}
         ></div>
         <div className="h-16 text-5xl font-bold leading-16 font-netmarbleB">
-          {props.name }
+          {props.name}
         </div>
       </div>
     </div>
