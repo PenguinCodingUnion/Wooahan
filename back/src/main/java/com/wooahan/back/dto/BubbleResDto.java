ef727825535499fa53632d168960da497b320b1d
이상민 <lsms3723@daum.net>
2023-05-08 17:28:40 +0900

    [FIX] getter

--- back/src/main/java/com/wooahan/back/dto/BubbleResDto.java
+++ back/src/main/java/com/wooahan/back/dto/BubbleResDto.java
@@ -3,11 +3,12 @@ package com.wooahan.back.dto;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
+import lombok.Getter;
 
 import java.util.List;
 
 @Builder
-@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
+@Getter
 public class BubbleResDto {
     private String answer;
     private String answerImg;
--- back/src/main/java/com/wooahan/back/dto/RunResDto.java
+++ back/src/main/java/com/wooahan/back/dto/RunResDto.java
@@ -2,10 +2,12 @@ package com.wooahan.back.dto;
 
 import com.wooahan.back.service.RunService;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 import java.util.List;
 
 @AllArgsConstructor
+@Getter
 public class RunResDto {
 
     private String quiz;
