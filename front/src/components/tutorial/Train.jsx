3388e707e12f3cf3aed28199b1ce4606ddae0bb5
조원희 <penguin1998@daum.net>
2023-05-09 17:57:56 +0900

    [Feat] 기차 튜토리얼 디자인 결정

--- front/src/components/tutorial/Train.jsx
+++ front/src/components/tutorial/Train.jsx
@@ -2,27 +2,28 @@ import PropTypes from "prop-types";
 import React from "react";
 import { connect } from "react-redux";
 import train1 from "assets/images/tutorials/train1.png";
-import pickhand from "assets/images/tutorials/pickHand.png";
 
 export const Train = (props) => {
   return (
-    <div>
-      <div className="absolute bg-mint w-28 h-11 right-7 top-7 font-MaplestoryLight text-2xl leading-[2.75rem]">
-        다음
+    <div className="relative h-full w-full overflow-hidden">
+      <div>
+        <div className="rounded-full left-1/2 bottom-0 absolute h-48 w-48 shadow-[0_-1000px_0_1000px_rgba(169,169,169,0.4)]">
+          <div className="rounded-full mx-auto m-12 h-24 w-24 bg-mainRed-200 animate-ping"></div>
+        </div>
+        <div className=" absolute top-6 flex w-full ">
+          <div className="bg-white rounded-md px-5 py-2 border mx-auto font-netmarbleB text-3xl ">
+            <p className="whitespace-nowrap">
+              끝 글자와 맞는 단어를 선택해주세요
+            </p>
+          </div>
+          <div className=" bg-mint w-28 h-12 mr-6 font-MaplestoryBold text-2xl leading-[3rem]">
+            <p>다음</p>
+          </div>
+        </div>
       </div>
+
       <div className="grid justify-center h-screen items-center ">
         <img className="" src={train1} alt="기차설명1" />
-        {/* <div className="absolute bg-mainSlate-300 top-1/4 left-1/2 rounded-xl bg-opacity-60 font-netmarbleM text-3xl ">
-          <p>단어의 끝 글자에요</p>
-        </div> */}
-        <div className="absolute bg-mainSlate-300 top-2/4 left-1/2 rounded-xl bg-opacity-60 font-netmarbleM text-3xl ">
-          <p>끝 글자와 맞는 단어를 선택해주세요</p>
-        </div>
-        <img
-          className="absolute w-28 h-28 top-3/4 left-2/3"
-          src={pickhand}
-          alt="손"
-        />
       </div>
     </div>
   );
