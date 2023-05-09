6224895cbc63c7a6701ca86afcf94446a156e062
이상민 <lsms3723@daum.net>
2023-05-10 03:04:39 +0900

    [Fix]swagger

--- back/src/main/java/com/wooahan/back/config/SwaggerConfig.java
+++ back/src/main/java/com/wooahan/back/config/SwaggerConfig.java
@@ -6,9 +6,12 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Server;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 
 @Configuration  //스프링 실행시 설정파일 읽어드리기 위한 어노테이션
@@ -17,11 +20,13 @@ public class SwaggerConfig {
 
     @Bean
     public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
+        Server serverLocal = new Server("local", "http://localhost:9090", "for local usages", Collections.emptyList(), Collections.emptyList());
+        Server testServer = new Server("test", "https://k8b206.p.ssafy.io", "for testing", Collections.emptyList(), Collections.emptyList());
+        return new Docket(DocumentationType.OAS_30)
+                .servers(serverLocal,testServer)
                 .apiInfo(apiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
                 .paths(PathSelectors.any())
                 .build();
     }
