8e49b34201f6f32673e6fc9194732bfe48996142
이상민 <lsms3723@daum.net>
2023-05-10 09:25:41 +0900

    [fix]오타

--- back/src/main/java/com/wooahan/back/controller/LoginController.java
+++ back/src/main/java/com/wooahan/back/controller/LoginController.java
@@ -28,8 +28,6 @@ public class LoginController {
         return new ResponseEntity<>(loginService.tempLogin(loginReqDto), HttpStatus.OK);
     }
 
-
-    @PostMapping("/info")
     //https://accounts.google.com/o/oauth2/auth?client_id=658207955186-n84qpvfhtdi82n6mfvbmh6v99aevulv7.apps.googleusercontent.com&redirect_uri=http://k8b206.p.ssafy.io/api/login/oauth2/code/google&response_type=code&scope=https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile
     @Operation(summary = "구글 oauth2(신경 안써도됨)", description = "구글 로그인 버튼 누르면 email,provider(google),name 줄거임")
     @GetMapping("/oauth2/code/{registrationId}")
