3090dea7dcf6ba52fbbeaf8ac0382c1407af3098
Jihoon <kjh8673a@naver.com>
2023-05-12 09:28:39 +0900

    [Test] 맞았습니다 gif 테스트

--- front/src/components/gameBubble/AnswerCard.jsx
+++ front/src/components/gameBubble/AnswerCard.jsx
@@ -4,13 +4,23 @@ import correct from "assets/sounds/correct.wav";
 import wrong from "assets/sounds/wrong.wav";
 import openedSeashell from "assets/images/bubble/opened_seashell.png";
 import closedSeashell from "assets/images/bubble/closed_seashell.png";
-import effectSound from 'util/effectSound';
+import effectSound from "util/effectSound";
 
 export const AnswerCard = (props) => {
   const [isOpened, setIsOpened] = useState(false);
+  const [answerGif, setAnswerGif] = useState("");
+  const [answerText, setAnswerText] = useState("");
 
   const es_correct = effectSound(correct, 1);
   const es_wrong = effectSound(wrong, 1);
+  const gif = [
+    require("assets/images/penguel_swing.gif"),
+    require("assets/images/penguel_hiphop.gif"),
+  ];
+  const text = [
+    "맞았습니다",
+    "틀렸습니다",
+  ];
 
   useEffect(() => {
     setTimeout(() => {
@@ -18,12 +28,16 @@ export const AnswerCard = (props) => {
     }, 500);
     if (props.name === props.answer) {
       es_correct.play();
+      setAnswerGif(gif[0]);
+      setAnswerText(text[0]);
       setTimeout(() => {
         props.closeCard();
         props.changeQuiz();
       }, 3000);
     } else if (props.name !== props.answer) {
       es_wrong.play();
+      setAnswerGif(gif[1]);
+      setAnswerText(text[1]);
       setTimeout(() => {
         props.closeCard();
       }, 3000);
@@ -32,7 +46,7 @@ export const AnswerCard = (props) => {
 
   return (
     <>
-      <div className="grid w-screen h-screen absolute top-0">
+      <div className="absolute top-0 grid w-screen h-screen">
         <div className="relative grid items-center self-center h-3/4">
           {!isOpened && (
             <img
@@ -48,6 +62,14 @@ export const AnswerCard = (props) => {
                 src={openedSeashell}
                 alt="img"
               />
+              <div className="absolute z-10 w-1/2 h-full -bottom-10 -left-20">
+                <img
+                className=""
+                  src={answerGif}
+                  alt=""
+                />
+                <div className="text-center whitespace-nowrap"><span className="px-5 py-3 text-2xl bg-[#964b00] border-4 border-[#c68a12] font-netmarbleB rounded-xl text-mainWhite tracking-wider">{answerText}</span></div>
+              </div>
               <div className="absolute grid grid-rows-4 -ml-40 overflow-hidden shadow-lg bg-mainPink-200 justify-items-center h-80 rounded-3xl w-80 shadow-mainBlack left-1/2">
                 <div className="grid self-end row-span-3">
                   <div
