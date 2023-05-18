27c23d014e9af982ee47935e4d7f40621c4bf0d2
Heesoo Choi <102863300+heesootory@users.noreply.github.com>
2023-05-18 17:51:47 +0900

    [fix] kakaoLogin fix

--- front/src/components/main/modal/KakaoLogin.js
+++ front/src/components/main/modal/KakaoLogin.js
@@ -3,7 +3,7 @@ import kakaoButton from "assets/images/loginButton/kakaoLogin.png";
 
 const KakaoLogin = () => {
     const Rest_api_key = "d313cf6ba97344781a1976206aed6112"
-    const redirect_uri = "http://localhost:3000/main"
+    const redirect_uri = "https://k8b206.p.ssafy.io/main"
     const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${Rest_api_key}&redirect_uri=${redirect_uri}&response_type=code`
     const handleLogin = ()=>{
         window.location.href = kakaoURL
