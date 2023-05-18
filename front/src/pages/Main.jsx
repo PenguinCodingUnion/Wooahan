6e13efc63eaa81938756c00637aba4381043d35f
Heesoo Choi <102863300+heesootory@users.noreply.github.com>
2023-05-18 13:18:16 +0900

    [feat] ui button

--- front/src/pages/Main.jsx
+++ front/src/pages/Main.jsx
@@ -70,7 +70,7 @@ export const Main = () => {
       <FallingAnimate falling={page} />
       {showModal && <Modal config={"setting"} current={"main"} />}
       <img className={`absolute z-0 w-screen h-screen opacity-90 ${coverImages[page]}`} />
-      <Header titleIsVisible={true} topLeftButton={"books"} />
+      <Header titleIsVisible={true} topLeftButton={"books"} page={page} />
       <Carousel />
     </div>
   );
