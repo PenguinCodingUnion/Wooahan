bd749539d14431498f9b0579f1de839d9db4e45a
jiwoo lee <its19447@gmail.com>
2023-05-18 11:08:34 +0900

    login test

--- front/src/components/main/modal/LoginButton.js
+++ front/src/components/main/modal/LoginButton.js
@@ -37,19 +37,19 @@ const LoginButton = () => {
   };
   //onClick={login}
   return (
-    <div className="flex justify-center h-[60%] mt-[1%]">
-      <div className="flex h-[70%] w-[60%] bg-white border border-black rounded-3xl">
-        <div className="flex justify-center items-center w-[20%]">
+    <div className="flex justify-center h-[60%] mt-[1%] pb-4">
+      {/* <div className="flex h-[70%] w-[60%] bg-white border border-black rounded-3xl"> */}
+      {/* <div className="flex justify-center items-center w-[20%]">
           <img src={googleIcon} className="h-[70%] rounded-3xl" />
-        </div>
-        <div className="flex justify-center items-center w-[80%]">
-          <div className="font-['MaplestoryOTFBold'] text-xl">
+        </div> */}
+      <div className="flex justify-center items-center w-[80%]">
+        {/* <div className="font-['MaplestoryOTFBold'] text-xl">
             구글 계정으로 계속하기
-          </div>
-          <div ref={loginBtn}></div>
-        </div>
-        <div className="w-[10%]"></div>
+          </div> */}
+        <div ref={loginBtn}></div>
       </div>
+      {/* <div className="w-[10%]"></div> */}
+      {/* </div> */}
     </div>
   );
 };
