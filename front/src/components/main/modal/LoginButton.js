3560d70a44b8d4454dc35520bf3fda3bff00afdc
jiwoo lee <its19447@gmail.com>
2023-05-18 09:51:31 +0900

    [Feat] for merge

--- front/src/components/main/modal/LoginButton.js
+++ front/src/components/main/modal/LoginButton.js
@@ -1,30 +1,57 @@
-import googleIcon from "assets/images/logo/googleIcon.png" 
+/* eslint-disable no-undef */
+import googleIcon from "assets/images/logo/googleIcon.png";
+import { useEffect } from "react";
 
-const url = "https://accounts.google.com/o/oauth2/auth/oauthchooseaccount?client_id=658207955186-n84qpvfhtdi82n6mfvbmh6v99aevulv7.apps.googleusercontent.com&redirect_uri=http%3A%2F%2Fk8b206.p.ssafy.io%2Fapi%2Flogin%2Foauth2%2Fcode%2Fgoogle&response_type=code&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email%20https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.profile&service=lso&o2v=1&flowName=GeneralOAuthFlow"
+const url =
+  "https://accounts.google.com/o/oauth2/auth/oauthchooseaccount?client_id=658207955186-n84qpvfhtdi82n6mfvbmh6v99aevulv7.apps.googleusercontent.com&redirect_uri=http%3A%2F%2Fk8b206.p.ssafy.io%2Fapi%2Flogin%2Foauth2%2Fcode%2Fgoogle&response_type=code&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email%20https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.profile&service=lso&o2v=1&flowName=GeneralOAuthFlow";
 
 const LoginButton = () => {
+  useEffect(() => {
+    gapi.load("auth2", () => {
+      gapi.auth2.init({
+        client_id:
+          "658207955186-n84qpvfhtdi82n6mfvbmh6v99aevulv7.apps.googleusercontent.com",
+      });
+    });
+  }, []);
 
-    const login = () => {
-        if(window.google_login) {
-            const goo = window.google_login.googleLogin();
-            console.log(goo)
-            return goo;
-        }
+  const login = async () => {
+    // if (window.google_login) {
+    //   const goo = window.google_login.googleLogin();
+    //   console.log(goo);
+    //   return goo;
+    // }
+
+    try {
+      const auth2 = gapi.auth2.getAuthInstance();
+      const googleUser = await auth2.signIn();
+
+      // Get the ID token
+      const id_token = googleUser.getAuthResponse().id_token;
+
+      // Here you can send id_token to your backend and validate it,
+      // or you can handle it in your app.
+      console.log("id_token", id_token);
+    } catch (error) {
+      console.log("Login failed", error);
     }
+  };
 
-    return(
-            <div onClick={login} className="flex justify-center h-[60%] mt-[1%]">
-                <div className="flex h-[70%] w-[60%] bg-white border border-black rounded-3xl">
-                    <div className="flex justify-center items-center w-[20%]">
-                        <img src={googleIcon} className="h-[70%] rounded-3xl"/>
-                    </div>
-                    <div className="flex justify-center items-center w-[80%]">
-                        <div className="font-['MaplestoryOTFBold'] text-xl">구글 계정으로 계속하기</div>
-                    </div>
-                    <div className="w-[10%]"></div>
-                </div>
-            </div>
-    )
-}
+  return (
+    <div onClick={login} className="flex justify-center h-[60%] mt-[1%]">
+      <div className="flex h-[70%] w-[60%] bg-white border border-black rounded-3xl">
+        <div className="flex justify-center items-center w-[20%]">
+          <img src={googleIcon} className="h-[70%] rounded-3xl" />
+        </div>
+        <div className="flex justify-center items-center w-[80%]">
+          <div className="font-['MaplestoryOTFBold'] text-xl">
+            구글 계정으로 계속하기
+          </div>
+        </div>
+        <div className="w-[10%]"></div>
+      </div>
+    </div>
+  );
+};
 
 export default LoginButton;
